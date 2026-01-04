a64f725611c6e27cba3dfab2db65791584b04a23
moabia1 <mdmoabia81@gmail.com>
2026-01-05 00:13:33 +0530

    workflow Dialog top section completed now we have to add form with shadcn and zod

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -2,7 +2,8 @@ import { GetWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
 import { Skeleton } from "@/components/ui/skeleton";
 import React, { Suspense } from "react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, InboxIcon } from "lucide-react";
+import CreateWorkflowDialog from "./_components/CreateWorkflowDialog";
 
 function page() {
   return (
@@ -12,6 +13,7 @@ function page() {
           <h1 className="text-3xl font-bold">Workflows</h1>
           <p className="text-muted-foreground">Manage your workflows</p>
         </div>
+        <CreateWorkflowDialog/>
       </div>
 
       <div className="h-full py-6">
@@ -40,11 +42,29 @@ async function UserWorkflows() {
       <Alert variant={"destructive"}>
         <AlertCircle className="w-4 h-4" />
         <AlertTitle>Error</AlertTitle>
-        <AlertDescription></AlertDescription>
+        <AlertDescription>
+          Something went wrong. please try again later
+        </AlertDescription>
       </Alert>
     );
   }
-  return <div></div>;
+
+  if (workflows.length === 0) {
+    return (
+      <div className="flex flex-col gap-4 h-full items-center justify-center">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <InboxIcon className="stroke-primary" size={40}/>
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">No workflow created yet</p>
+          <p className="text-sm text-muted-foreground">Click the button below to create your first workflow</p>
+        </div>
+        <CreateWorkflowDialog triggerText="Create your first workflow"/>
+      </div>
+    );
+  }
+
+  return <div></div>
 }
 
 export default page;
---
+++ components/CustomDialogHeader.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import { DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {Separator} from "@/components/ui/separator"
+
+interface Props {
+  title?: string;
+  subtitle?: string;
+  icon?: LucideIcon;
+
+  iconClassName?: string;
+  titleClassName?: string;
+  subtitleClassName?: string;
+}
+
+function CustomDialogHeader(props: Props) {
+  const Icon = props.icon;
+  return (
+    <DialogHeader className="py-6">
+      <DialogTitle asChild>
+        <div className="flex flex-col items-center gap-2 mb-2">
+          {props.icon && (
+            <props.icon
+              size={30}
+              className={cn("stroke-primary", props.iconClassName)}
+            />
+          )}
+          {props.title && (
+            <p className={cn("text-xl stroke-primary", props.titleClassName)}>
+              {props.title}
+            </p>
+          )}
+          {props.subtitle && (
+            <p className={cn("text-sm text-muted-foreground stroke-primary", props.subtitleClassName)}>
+              {props.subtitle}
+            </p>
+          )}
+        </div>
+      </DialogTitle>
+      <Separator/>
+    </DialogHeader>
+  );
+}
+
+export default CustomDialogHeader;
