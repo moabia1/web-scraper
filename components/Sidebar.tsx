3c329bba3cdc6dc96c36ca70f35eb14a6d78c8c4
moabia1 <mdmoabia81@gmail.com>
2026-01-04 10:22:24 +0530

    mobile sidebar completed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,13 +4,16 @@ import {
   CoinsIcon,
   HomeIcon,
   Layers2Icon,
+  MenuIcon,
   ShieldCheckIcon,
 } from "lucide-react";
-import React from "react";
+
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { usePathname } from "next/navigation";
+import { SheetContent, SheetTrigger, Sheet } from "./ui/sheet";
 
 const routes = [
   {
@@ -36,7 +39,6 @@ const routes = [
 ];
 
 function DesktopSidebar() {
-
   const pathname = usePathname();
 
   const activeRoutes =
@@ -44,7 +46,6 @@ function DesktopSidebar() {
       (route) => route.href.length > 0 && pathname.includes(route.href)
     ) || routes[0];
 
-  
   return (
     <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
       <div className="flex items-center justify-center gap-2 border-b-[1px] border-seperate p-4">
@@ -57,7 +58,10 @@ function DesktopSidebar() {
             key={route.href}
             href={route.href}
             className={buttonVariants({
-              variant: activeRoutes.href === route.href ? "sidebarActiveItem" : "sidebarItem"
+              variant:
+                activeRoutes.href === route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
             })}
           >
             <route.icon size={20} />
@@ -65,6 +69,53 @@ function DesktopSidebar() {
           </Link>
         ))}
       </div>
+    </div>
+  );
+}
+
+export function MobileSidebar() {
+  const pathname = usePathname();
+  const [isOpen, setOpen] = useState(false);
+
+  const activeRoutes =
+    routes.find((route) => route.href.length > 0 && route.href === pathname) ||
+    routes[0];
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            className="w-[400px] sm:w-[540px] space-y-4"
+            side={"left"}
+          >
+            <Logo />
+            <div className="flex flex-col gap-1">
+              {routes.map((route) => (
+                <Link
+                  key={route.href}
+                  href={route.href}
+                  className={buttonVariants({
+                    variant:
+                      activeRoutes.href === route.href
+                        ? "sidebarActiveItem"
+                        : "sidebarItem",
+                  })}
+                  onClick={() => setOpen((prev) => !prev)}
+                >
+                  <route.icon size={20} />
+                  {route.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </nav>
     </div>
   );
 }
