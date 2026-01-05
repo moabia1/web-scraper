b09013735b5dbc728a7c6f6cc5d3e770e72a0ced
moabia1 <mdmoabia81@gmail.com>
2026-01-05 11:15:57 +0530

    create workflow form UI complete

--- app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -1,29 +1,99 @@
 "use client";
 
-import React, { useState } from 'react'
-import {Dialog,DialogContent,DialogTrigger} from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button';
-import { Layers2Icon } from 'lucide-react';
-import CustomDialogHeader from '@/components/CustomDialogHeader';
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Layers2Icon } from "lucide-react";
+import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { createWorkflowSchema } from "@/schema/workflow";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormDescription,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
-function CreateWorkflowDialog({triggerText}:{triggerText?:string}) {
-  const [open, setOpen] = useState(false)
+
+function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof createWorkflowSchema>>({
+    resolver: zodResolver(createWorkflowSchema),
+    defaultValues: {},
+  });
+
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button>
-          {triggerText ?? "Create workflow"}
-        </Button>
+        <Button>{triggerText ?? "Create workflow"}</Button>
       </DialogTrigger>
-      <DialogContent className='px-0'>
+      <DialogContent className="px-0">
         <CustomDialogHeader
           icon={Layers2Icon}
           title="Create Workflow"
           subtitle="start building your workflow"
         />
+        <div className="p-6">
+          <Form {...form}>
+            <form className="space-y-8 w-full">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex gap-1 items-center">
+                      Name
+                      <p className="text-xs text-primary">(required)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Choose a descriptive and unique name
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex gap-1 items-center">
+                      Description
+                      <p className="text-xs text-primary">(optional)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea className="resize-none" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Provide a brief description of what your workflow does.
+                      <br /> This is optional but can help you remember the workflow's purpose.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full" type="submit">
+                Proceed
+              </Button>
+            </form>
+          </Form>
+        </div>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
 
-export default CreateWorkflowDialog
+export default CreateWorkflowDialog;
